65648043dc01521e16ac830c559c3519839a3332
mesfen <mesfen145mogess@gmail.com>
2022-06-15 11:34:01 +0300

    argument

---
+++ argument.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * main - teking argument
+ *
+ * Return: 0
+ */
+
+int main( int argc, char *argv[] )  {
+
+	   if( argc == 2 ) {
+		         printf("The argument supplied is %s\n", argv[1]);
+			    }
+	      else if( argc > 2 ) {
+		            printf("Too many arguments supplied.\n");
+			       }
+	         else {
+			       printf("One argument expected.\n");
+			          }
+}
